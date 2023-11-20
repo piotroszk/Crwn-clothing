d18e0e02553b19a4c40bf28b8d27ca2cf09c888c
Piotr Roszkowski <pro@novicell.co.uk>
2023-11-20 12:41:39 +0100

    cart total and checkout completed

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,17 +4,24 @@ import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout')
+    }
+
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
                 {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
             </div>
-            <Button>Go to checkout</Button>
+            <Button onClick={goToCheckoutHandler}>Go to checkout</Button>
         </div>
     )
 }
