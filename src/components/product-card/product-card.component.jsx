6e31690a75f1c2cd01e1f1d0914baa59d13660ed
Piotr Roszkowski <pro@novicell.co.uk>
2023-11-20 11:48:23 +0100

    cart item

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,15 @@
 import './product-card.styles.scss'
+import { useContext } from 'react';
 
 import Button from '../button/button.component'
+import { CartContext } from '../../contexts/cart.context';
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCard = () => addItemToCart(product);
+
     return (
         <div className='product-card-container'>
             <img src={imageUrl} alt={``} />
@@ -11,7 +17,7 @@ const ProductCard = ({ product }) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted'>Add to card</Button>
+            <Button buttonType='inverted' onClick={addProductToCard}>Add to card</Button>
         </div>
     )
 }
